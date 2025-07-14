a7efbdb4cd685346c02bbfcacf896df1f463c704
AlirezaAbedini04 <alirezaabedini119@gmail.com>
2025-07-14 20:28:29 +0330

    finding files and writing them in backup file

--- os_backup.sh
+++ os_backup.sh
@@ -2,3 +2,18 @@
 
 read -p "Enter the path for backup: " search_path
 read -p "Enter the format of the files(txt, jpg,..: " file_ext
+
+if [ ! -d "$search_path" ]; then
+    echo "path does not exist"
+    exit 1
+fi
+
+
+config_file="backup.conf"
+> "$config_file"  
+
+
+find "$search_path" -type f -name "*.$file_ext" >> "$config_file"
+
+
+echo "List of .$file_ext files in $search_path has been saved to $config_file."
